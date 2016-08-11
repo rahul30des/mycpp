fd35722037175a65b651e701f66d20f4d5ee1b5c
Rahul Deshpande <rahul30des@users.noreply.github.com>
2016-08-11 10:48:42 -0700

    Add files via upload

--- balancedParanthesis/braceCheck.cpp
+++ balancedParanthesis/braceCheck.cpp
@@ -7,35 +7,42 @@
 #include <iostream>
 #include <algorithm>
 #include <unordered_map>
+#include <list>
 
 using namespace std;
 
 
-vector <bool> brace_check(vector < string > values) {
+vector <bool> brace_check(list < string > values) {
     vector <bool> result;
-    vector <char> stk;
-    vector <char> in = {'{', '(', '['};
-    vector <char> out = {'}', ')', ']'};
+    stack <char> stk;
+    list <char> in = {'{', '(', '['};
+    list <char> out = {'}', ')', ']'};
                 
 
-    for(vector<string>::iterator it = values.begin(); it != values.end(); ++it) {
+    for(list <string>::iterator it = values.begin(); it != values.end(); ++it) {
         string temp = *it;
         bool flag = true;
         int i = 0;
-        stk.clear();
+        stk = stack<char> ();
         for(; i < temp.length(); i++) {
             char ch = temp[i];
 
             auto iti = find(in.begin(), in.end(), ch);
             if(iti != in.end()) {
-                stk.push_back(ch);
+                stk.push(ch);
                 continue;
             } 
 
             auto ito = find(out.begin(), out.end(), ch);
             if (ito != out.end()) {
-                char top = stk.back();
-                stk.pop_back();
+                if(stk.empty()) {
+                    result.push_back(false);
+                    break;
+                }
+                char top = stk.top();
+                stk.pop();
+                // Ideally should have been a check on the out index equivalent
+                // of top and ch 
                 if (!((ch == '}' && top == '{') || (ch == ']' && top == '[')|| (ch == ')' && top == '('))) {
                     result.push_back(false);
                     break;
@@ -59,7 +66,7 @@ vector <bool> brace_check(vector < string > values) {
 // Main Function
 
 int main() {
-    vector <string> values = {"{}", "{"};
+    list <string> values = {"{}", "{"};
     vector <bool> out;
     out = brace_check(values);
     for(vector <bool>::iterator it = out.begin(); it != out.end(); ++it) {
