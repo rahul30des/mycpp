d2f33397ef1c6a8251e734e856030cb0b9d3fa5f
Rahul Deshpande <rahul30des@gmail.com>
2021-09-25 13:40:41 -0700

    First Solution

--- lexographical_sort/lex_sort.cpp
+++ lexographical_sort/lex_sort.cpp
@@ -1,26 +1,29 @@
 #include<iostream>
-#include<string>
 #include<algorithm>
+#include<string>
 #include<vector>
 
 using namespace std;
 
+// Main Function
 int main() {
-    int size = 0;
-    cin>>size;
-    vector <string> str(size);
-    // Input - O(n)
-    for(int i = 0; i < size; i++) {
-        string temp;
-        cin>>temp;
-        str.push_back(temp);
-    }
+     int size = 0;
+     cin>>size;
+     vector <string> vstr(size);
+    
+     for(int i = 0; i < size; i++) {
+         string str;
+         cin>>str;
+         vstr.push_back(str);
+     }
+     sort(vstr.begin(), vstr.end());
 
-    // Sorting - Time Complexity O(nlogn)
-    sort(str.begin(), str.end());
+     for(vector<string>::iterator it = vstr.begin(); it !=vstr.end(); it++) {
+         string str = *it;
+         cout<<str<<endl;
+     }
 
-    for(auto it = str.begin(); it != str.end(); it++) {
-        cout<<endl<<*it;
-    }
-    return 0;
+     return 0;
 }
+
+
