ce6189318d4549b49e1345093e53076e2aa36e2e
Rahul Deshpande <rahul30des@gmail.com>
2021-09-25 19:34:25 -0700

    Find LCA, path between LCA and nodes

--- path2nodesBT/path2nodesBT.cpp
+++ path2nodesBT/path2nodesBT.cpp
@@ -1,104 +1,77 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-
-typedef struct node
-{
-    struct node *left, *right;
+typedef struct node{
     int val;
+    struct node *left, *right;
 }NODE;
 
-NODE* newNode(int val)
-{
-    NODE*temp = new NODE;
-    temp->val = val;
-    temp->left = temp->right = NULL;
-    return temp;
-}
+NODE* NewNode(int value) {
+    NODE *root = new NODE;
+    root->val = value;
+    root->left = root->right = NULL;
+    return root;
+}    
 
-
-/* 
- * Driver func returns LCA of two values n1 and n2
- * v1 is set as true by this function if n1 is found
- * v2 is set as true by this function if n2 is found 
- */
-NODE* findLCAUtil(NODE* root, int n1, int n2, bool &v1, bool &v2)
-{
-    if (root == NULL) {
+NODE* FindLCAUtil(NODE* root, int v1, int v2, bool& n1, bool& n2) {
+    if(root == NULL) {
         return NULL;
     }
 
-    // If either n1 or n2 matches with root's val, mark the presence
-    if (root->val == n1)
-    {
-        v1 = true;
+    if(root->val == v1) {
+        n1 = true;
         return root;
     }
 
-    if (root->val == n2)
-    {
-        v2 = true;
+    if(root->val == v2) {
+        n2 = true;
         return root;
     }
 
-    // Look for vals in left and right subtrees
-    NODE* left_lca = findLCAUtil(root->left, n1, n2, v1, v2);
-    NODE* right_lca = findLCAUtil(root->right, n1, n2, v1, v2);
+    NODE* left_lca = FindLCAUtil(root->left, v1, v2, n1, n2); 
+    NODE* right_lca = FindLCAUtil(root->right, v1, v2, n1, n2); 
 
-    // Both values are present in different subtrees
-    if (left_lca && right_lca) { 
+    if(left_lca && right_lca) {
         return root;
-    }
-
-    return (left_lca != NULL) ? left_lca: right_lca;
+    } 
+    return (left_lca?left_lca:right_lca);    
 }
 
-bool findNODE(NODE* root, int val)
-{
-    if (root == NULL) {
+bool FindNode(NODE* root, int value) {
+    if(root == NULL) {
         return false;
     }
-
-    if (root->val == val || findNODE(root->left, val) || findNODE(root->right, val)) {
-        return true;
-    }
-
-    return false;
+    return ((root->val == value) || FindNode(root->left, value) || FindNode(root->right, value));
 }
 
-// Unoptimized Code for now
-void createPath(NODE* root, int val, vector <int> &stk)
-{
+NODE* FindLCA(NODE* root, int v1, int v2) {
+    bool n1 = false, n2 = false;
+    NODE* lca = FindLCAUtil(root, v1, v2, n1, n2);
+    if((n1 && n2) || (n1 && FindNode(lca, v2)) || (n2 && FindNode(lca, v1))) {
+        return lca;
+    } else {
+        return NULL;
+    }
+}
+
+
+void CreatePath(NODE* root, int value, vector <int>& stk) {
     if (root == NULL) {
         return;
     }
 
     stk.push_back(root->val);
-    if (root->val == val) {
+    if (root->val == value) {
         return;
-    } else if (findNODE(root->left, val)) {
-        createPath(root->left, val, stk);
-    } else if (findNODE(root->right, val)) {
-        createPath(root->right, val, stk);
+    } else if (FindNode(root->left, value)) {
+        CreatePath(root->left, value, stk);
+    } else if (FindNode(root->right, value)) {
+        CreatePath(root->right, value, stk);
     }
-}
-
-// Uses Driver func and checks (for both nodes in same subtree)
-NODE* findLCA(NODE* root, int n1, int n2)
-{
-    bool v1 = false, v2 = false;
-
-    NODE* lca = findLCAUtil(root, n1, n2, v1, v2);
-
-    // Return LCA only if both n1 and n2 are present in tree
-    if ((v1 && v2) || (v1 && findNODE(lca, n2)) || (v2 && findNODE(lca, n1))) {
-        return lca;
-    }
-
-    return NULL;
 }
 
 // Main
@@ -107,22 +80,22 @@ int main() {
     int v2 = 0;
 
     // Binary Tree creation
-    NODE* root = newNode(0);
-    root->left = newNode(1);
-    root->right = newNode(2);
-    root->left->left = newNode(3);
-    root->left->right = newNode(4);
-    root->right->left = newNode(5);
-    root->right->right = newNode(6);
-    root->left->left->left = newNode(7);
-    root->left->right->left = newNode(8);
-    root->right->left->left = newNode(9);
-    root->right->left->right = newNode(10);
-    root->right->right->right = newNode(11);
+    NODE* root = NewNode(0);
+    root->left = NewNode(1);
+    root->right = NewNode(2);
+    root->left->left = NewNode(3);
+    root->left->right = NewNode(4);
+    root->right->left = NewNode(5);
+    root->right->right = NewNode(6);
+    root->left->left->left = NewNode(7);
+    root->left->right->left = NewNode(8);
+    root->right->left->left = NewNode(9);
+    root->right->left->right = NewNode(10);
+    root->right->right->right = NewNode(11);
 
     cin>>v1>>v2;
 
-    NODE* lca = findLCA(root, v1, v2);
+    NODE* lca = FindLCA(root, v1, v2);
     if (lca != NULL) {
         cout << "LCA("<<v1<<","<<v2<<") = " << lca->val;
     }
@@ -132,18 +105,19 @@ int main() {
     }
 
     vector <int> stk;
-    createPath(lca, v1, stk);
+    CreatePath(lca, v1, stk);
     cout<<endl;
     while (!stk.empty()) {
         cout<<stk.back()<<"\t";
         stk.pop_back();
     }
     stk.clear();
-    createPath(lca, v2, stk);
+    CreatePath(lca, v2, stk);
     stk.erase(stk.begin());
     for(auto it=stk.begin(); it != stk.end(); it++) {
         cout<<*it<<"\t";
     }
+    cout<<endl;
 
     return 0;
 }
